57eb0cd24c03072600f0183ee28a02c60ed49771
leviettho0611 <leviettho0611@gmail.com>
2024-03-25 19:02:06 +0700

    API

--- BE/database/migrations/2014_10_12_000000_create_users_table.php
+++ BE/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name');
+            $table->enum('sex', ['male', 'female', 'pending'])->default('pending');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('role', ['admin', 'user', 'coach'])->default('user');
+            $table->integer('payment_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- BE/routes/api.php
+++ BE/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/loadModel', [\App\Http\Controllers\Controller::class, 'loadModel']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::post('/change-pass', [AuthController::class, 'changePassWord']);    
+});
